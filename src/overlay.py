b69b4df45af16035878c53621577b6d255e2c917
FixedOctocat <43385721+FixedOctocat@users.noreply.github.com>
2022-02-07 14:48:27 +0300

    буковки прозрачные и двигаются, кружка еще нет

--- src/overlay.py
+++ src/overlay.py
@@ -4,6 +4,7 @@ import json
 import win32pipe
 import win32file
 import pywintypes
+import PySimpleGUI as sg
 
 
 def get_data():
@@ -33,5 +34,45 @@ def get_data():
                 quit = True
 
 
+def main():
+    text_gmp = "Gold per minute:"
+    text_gold_crips = "Gold from crips:"
+    text_gold_heroes = "Gold from heroes:"
+    text_gold_passive = "passive gold:"
+
+    sg.SetOptions(margins=(0, 0), element_padding=(0, 0))
+
+    layout = [
+        [
+            sg.Graph(
+                canvas_size=(1500, 300),
+                graph_bottom_left=(0, 0),
+                graph_top_right=(0, 0),
+                background_color="red",
+                key="graph",
+            )
+        ],
+        [sg.Text(text_gmp)],
+        [sg.Text(text_gold_crips)],
+    ]
+
+    window = sg.Window(
+        "Graph test",
+        layout,
+        background_color="red",
+        transparent_color="red",
+        no_titlebar=True,
+        alpha_channel=0.5,
+        grab_anywhere=True,
+        keep_on_top=True,
+    ).Finalize()
+
+    graph = window.FindElement("graph")  # type: sg.Graph
+    while True:
+        event, values = window.Read()
+        if event is None:
+            break
+
+
 if __name__ == "__main__":
-    print(get_data())
+    main()
