df3a9427bd03ad8d75079c72ec485177a8f273f3
FightinFrog <51506313+FightinFrog@users.noreply.github.com>
2022-02-09 12:44:03 +0300

    1 таск, нужно тестить

--- src/overlay.py
+++ src/overlay.py
@@ -1,45 +1,175 @@
 import PySimpleGUI as sg
+import math
 
 
-def main():
-    text_gmp = "Gold per minute:"
-    text_gold_crips = "Gold from crips:"
-    text_gold_heroes = "Gold from heroes:"
-    text_gold_passive = "passive gold:"
+def overlay_gold_helper():
+    text_gmp = "gpm :"
+    text_xpm = "xpm :"
+    GRAPH_SIZE = (500, 500)
+    DATA_SIZE = (500, 500)
+    GRAPH_SIZE_PIE_CHART = (500, 100)
+    BAR_SPACING = 40
+    EDGE_OFFSET = 3
+    VERTICAL_OFFSET = 70
 
-    sg.SetOptions(margins=(0, 0), element_padding=(0, 0))
+    HORIZONTAL_OFFSET = 43
+    BAR_WIDTH = 40
+    gold_from_crips = 10
+    gold_from_heroes = 300
+    passive_gold = 200
+    sum_gold = gold_from_heroes + gold_from_crips + passive_gold
 
+    sg.SetOptions(margins=(0,0), element_padding=(0,0))
+    graph = sg.Graph(GRAPH_SIZE, (0, 0), DATA_SIZE, background_color='red', key='-GRAPH-')
     layout = [
-        [
-            sg.Graph(
-                canvas_size=(1500, 300),
-                graph_bottom_left=(0, 0),
-                graph_top_right=(0, 0),
-                background_color="red",
-                key="graph",
-            )
-        ],
-        [sg.Text(text_gmp)],
-        [sg.Text(text_gold_crips)],
+        [graph],
+        [sg.Button(' pie chart '), sg.Button(' bar ')],
+        [sg.Button(' hide '), sg.Button(' exit ')]
     ]
 
-    window = sg.Window(
-        "Graph test",
-        layout,
-        background_color="red",
-        transparent_color="red",
-        no_titlebar=True,
-        alpha_channel=0.5,
-        grab_anywhere=True,
-        keep_on_top=True,
-    ).Finalize()
+    window = sg.Window('ищу тяночку, кандидатки звоните по телефону 89851144689', layout, background_color='red', transparent_color='red', no_titlebar=True, alpha_channel=.5, grab_anywhere=True, keep_on_top=True).Finalize()
 
-    graph = window.FindElement("graph")  # type: sg.Graph
     while True:
         event, values = window.Read()
-        if event is None:
+        if event is None or event == ' exit ':
             break
+        if event == ' bar ':
+            graph.Erase()
+            graph.DrawRectangle(top_left=(0, 70 + 500* max(gold_from_crips/sum_gold, gold_from_heroes/sum_gold, passive_gold/sum_gold)), bottom_right=(120, 0), fill_color='grey') ##общий фон
+            graph.DrawRectangle(top_left=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET, 500*(gold_from_crips)/(sum_gold) + VERTICAL_OFFSET), ##крипы
+                                bottom_right=(BAR_SPACING + EDGE_OFFSET + BAR_WIDTH - HORIZONTAL_OFFSET, VERTICAL_OFFSET),
+                                fill_color='pink')
+            graph.DrawText(text=gold_from_crips,
+                           color='black',
+                           location=(BAR_SPACING + 20 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 25))
+            graph.DrawText(text=round(gold_from_crips/sum_gold * 100),
+                           color='black',
+                           location=(BAR_SPACING + 20 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 15))
+            graph.DrawText(text='%',
+                           color='black',
+                           location=(BAR_SPACING + 30 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 15))
+
+
+            graph.DrawRectangle(top_left=(2 * BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET, 500*(gold_from_heroes)/(sum_gold) + VERTICAL_OFFSET), ##герои
+                                bottom_right=(2 * BAR_SPACING + EDGE_OFFSET + BAR_WIDTH - HORIZONTAL_OFFSET, VERTICAL_OFFSET),
+                                fill_color='green')
+            graph.DrawText(text=gold_from_heroes,
+                           color='black',
+                           location=(2 * BAR_SPACING + 20 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 25))
+            graph.DrawText(text=round(gold_from_heroes/sum_gold * 100),
+                           color='black',
+                           location=(2 * BAR_SPACING + 20 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 15))
+            graph.DrawText(text='%',
+                           color='black',
+                           location=(2 * BAR_SPACING + 30 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 15))
+
+            graph.DrawRectangle(top_left=(3 * BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET, 500*(passive_gold)/(sum_gold) + VERTICAL_OFFSET), ##пассивный доход
+                                bottom_right=(3 * BAR_SPACING + EDGE_OFFSET + BAR_WIDTH - HORIZONTAL_OFFSET, VERTICAL_OFFSET),
+                                fill_color='orange')
+            graph.DrawText(text=passive_gold,
+                           color='black',
+                           location=(3 * BAR_SPACING + 20 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 25))
+            graph.DrawText(text=round(passive_gold/sum_gold * 100),
+                           color='black',
+                           location=(3 * BAR_SPACING + 20 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 15))
+            graph.DrawText(text='%',
+                           color='black',
+                           location=(3 * BAR_SPACING + 30 - HORIZONTAL_OFFSET, VERTICAL_OFFSET + 15))
+
+            graph.DrawText(text=text_xpm,                           ## описание к графику
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 20, 15))
+            graph.DrawText(text='значение',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 20 + 45, 15))
+
+            graph.DrawText(text=text_gmp,
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 19, 25))
+            graph.DrawText(text='значение',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 19 + 45, 25))
+
+            graph.DrawText(text='orange : passive gold',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 59, 35))
+
+            graph.DrawText(text='green : from heroes',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 54.5, 45))
+
+            graph.DrawText(text='pink : from crips',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 48, 55))
+
+        if event == ' pie chart ':
+            graph.erase()
+            graph.DrawRectangle(top_left=(0,200), bottom_right=(120,0), fill_color='grey')
+            graph.DrawArc((0, 70), (DATA_SIZE[0] - 380, DATA_SIZE[1] - 300), extent=(gold_from_crips/sum_gold)*360, start_angle=0, arc_color='pink', fill_color='pink')
+            graph.DrawArc((0, 70), (DATA_SIZE[0] - 380, DATA_SIZE[1] - 300), extent=(gold_from_heroes/sum_gold)*360, start_angle=(gold_from_crips/sum_gold)*360, arc_color='green', fill_color='green')
+            graph.DrawArc((0, 70), (DATA_SIZE[0] - 380, DATA_SIZE[1] - 300), extent=(passive_gold/sum_gold)*360, start_angle=(gold_from_crips/sum_gold)*360 + (gold_from_heroes/sum_gold)*360, arc_color='orange', fill_color='orange')
+
+            graph.DrawText(text=gold_from_crips,
+                           color='black',
+
+                           location=(62 + 31 * math.cos((gold_from_crips/sum_gold) * math.pi),
+                                     136 + 31 * math.sin((gold_from_crips/sum_gold) * math.pi)))
+
+
+
+            graph.DrawText(text=gold_from_heroes,
+                           color='black',
+                           location=(62 + 31 * (math.cos((gold_from_heroes / sum_gold) * math.pi + (gold_from_crips/sum_gold)*math.pi*2)),
+                                     136 + 31 * (math.sin((gold_from_heroes / sum_gold) * math.pi + (gold_from_crips/sum_gold)*math.pi*2))))
+
+
+            graph.DrawText(text=passive_gold,
+                           color='black',
+                           location=(62 + 31 * (math.cos((passive_gold / sum_gold) * math.pi + (gold_from_crips/sum_gold)*math.pi*2 + (gold_from_heroes/sum_gold)*math.pi*2)),
+                                     136 + 31 * (math.sin((passive_gold / sum_gold) * math.pi + (gold_from_crips/sum_gold)*math.pi*2 + (gold_from_heroes/sum_gold)*math.pi*2))))
+
+
+            graph.DrawText(text=text_xpm,  ## описание к графику
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 20, 15))
+            graph.DrawText(text='значение',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 20 + 45, 15))
+
+            graph.DrawText(text=text_gmp,
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 19, 25))
+            graph.DrawText(text='значение',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 19 + 45, 25))
+
+            graph.DrawText(text='orange : passive gold',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 59, 35))
+
+            graph.DrawText(text='green : from heroes',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 54.5, 45))
+
+            graph.DrawText(text='pink : from crips',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 48, 55))
+
+        if event == ' hide ':
+            graph.erase()
+            graph.DrawRectangle(top_left=(0, 70), bottom_right=(120, 0), fill_color='grey')
+            graph.DrawText(text=text_xpm,  ## описание к графику
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 20, 15))
+            graph.DrawText(text='значение',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 20 + 45, 15))
+
+            graph.DrawText(text=text_gmp,
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 19, 25))
+            graph.DrawText(text='значение',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 19 + 45, 25))
+
+            graph.DrawText(text=passive_gold,
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 13, 35))
+            graph.DrawText(text=' : passive gold',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 59, 35))
+
+            graph.DrawText(text=gold_from_heroes,
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 13, 45))
+            graph.DrawText(text=' : from heroes',
+                               location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 58, 45))
+
+            graph.DrawText(text=gold_from_crips,
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 13, 55))
+            graph.DrawText(text=' : from crips',
+                           location=(BAR_SPACING + EDGE_OFFSET - HORIZONTAL_OFFSET + 53, 55))
+    window.Close()
 
 
 if __name__ == "__main__":
-    main()
+    overlay_gold_helper()
